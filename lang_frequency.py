6bc67771e3b5c71638e28648738d974ca376ceab
Evgeny Shestakov <evgeny.shestakov@gmail.com>
2017-08-01 09:11:03 +0000

    fix for review

--- lang_frequency.py
+++ lang_frequency.py
@@ -18,19 +18,20 @@ def get_most_frequent_words(text, max_values = 10):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        file_content = load_file(sys.argv[1])
-        try:
-            max_values = 10 if len(sys.argv) <= 2 else int(sys.argv[2])
-        except ValueError:
-            print('wrong max values format, please use float')
-            sys.exit(1)
-        for word, repeated in get_most_frequent_words(file_content,
-                                                      max_values):
-            print('word "{0}" repeated {1} times\n'.format(word, repeated))
-        sys.exit(0)
-    else:
+    if len(sys.argv) < 2:
         print('error: please add text file as argument: ' + 
             './python lang_frequency.py data.txt [max_values]')
-        sys.exit(1)          
+        sys.exit(1)  
+    
+    file_content = load_file(sys.argv[1])
+    try:
+        max_values = 10 if len(sys.argv) <= 2 else int(sys.argv[2])
+    except ValueError:
+        print('wrong max values format, please use float')
+        sys.exit(1)
+    for word, repeated in get_most_frequent_words(file_content,
+                                                  max_values):
+        print('word "{0}" repeated {1} times\n'.format(word, repeated))
+    sys.exit(0)
+                
         
